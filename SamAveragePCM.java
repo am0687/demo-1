fdbaf00522ca45aea9b761f459751f9f768b9a63
Aditi Muskan <muskan.aditi1@gmail.com>
2026-03-02 19:36:25 +0530

    level 1 q2

---
+++ SamAveragePCM.java
@@ -0,0 +1,16 @@
+public class SamAveragePCM {
+
+    public static void main(String[] args) {
+
+        // Given marks
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        // Calculate average percentage
+        double average = (maths + physics + chemistry) / 3.0;
+
+        // Display result
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
